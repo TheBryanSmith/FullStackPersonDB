c9d9edc908face145d2dd43c570df6cdd13e6c49
Bryan <bryan@Zipcoders-MacBook-Pro-10.local>
2024-09-17 16:56:45 -0400

    FrontEnd FUNCTIONALITY DOES NOT WORK AS INTENDED (IF AT ALL....) must seek further guidance  'I made a wrong turn back there somewhere -Erykah Badu'

---
+++ demo 2/src/main/resources/static/service.js
@@ -0,0 +1,64 @@
+class Person{
+    constructor(id, firstName, lastName){
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+   }
+}
+
+function updateDisplay(response) {
+    document.getElementById("output").innerText = JSON.stringify(response);
+    }
+
+function create(event) {
+event.preventDefault();
+const id = document.getElementById("person-id").value;
+const firstName = document.getElementById("FirstName").value;
+const lastName = document.getElementById("LastName").value;
+const person = new Person(id, firstName, lastName);
+const personData = JSON.stringify(person);
+
+$.ajax({
+    type: "POST",
+    crossDomain: true,
+    headers:{
+        'Accept' : 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    },
+    url: "/person-controller/create",
+    data: personData,
+    dataType: "JSON",
+    success: function(response) {
+        updateDisplay(response);
+    },
+    error: function(response) {
+        updateDisplay(response);
+    }
+ });
+}
+
+
+function read(event) {
+event.preventDefault();
+const id = document.getElementById("person-id").value;
+
+$.ajax({
+    type: "GET",
+    crossDomain: true,
+    headers:{
+        'Accept' : 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    },
+    url: "/person-controller/read".concat(id),
+    dataType: "JSON",
+    success: function(response) {
+        updateDisplay(response);
+    },
+    error: function(response) {
+        updateDisplay(response);
+    }
+ });
+}
