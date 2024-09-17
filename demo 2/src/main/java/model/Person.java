0b7ca53008e7248458c222d7333aaba91b1ac530
Bryan <bryan@Zipcoders-MacBook-Pro-10.local>
2024-09-17 01:38:34 -0400

    Setting up properties

--- demo 2/src/main/java/model/Person.java
+++ demo 2/src/main/java/model/Person.java
@@ -1,10 +1,14 @@
 package model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Person {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String fName;
     private String lName;
