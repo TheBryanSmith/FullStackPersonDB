412d18c316a4cbfa03ae618550af4eaf29215ed8
Bryan <bryan@Zipcoders-MacBook-Pro-10.local>
2024-09-17 17:20:17 -0400

    Finished with help of leon

--- demo 2/src/main/resources/static/service.js
+++ demo 2/src/main/resources/static/service.js
@@ -1,42 +1,41 @@
 class Person{
     constructor(id, firstName, lastName){
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
    }
 }
 
 function updateDisplay(response) {
     document.getElementById("output").innerText = JSON.stringify(response);
-    }
+}
 
 function create(event) {
-event.preventDefault();
-const id = document.getElementById("person-id").value;
-const firstName = document.getElementById("FirstName").value;
-const lastName = document.getElementById("LastName").value;
-const person = new Person(id, firstName, lastName);
-const personData = JSON.stringify(person);
+    event.preventDefault();
+    const id = document.getElementById("person-id").value;
+    const firstName = document.getElementById("first-name").value;
+    const lastName = document.getElementById("last-name").value;
+    const person = new Person(id, firstName, lastName);
+    const personData = JSON.stringify(person);
 
-$.ajax({
-    type: "POST",
-    crossDomain: true,
-    headers:{
-        'Accept' : 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    },
-    url: "/person-controller/create",
-    data: personData,
-    dataType: "JSON",
-    success: function(response) {
-        updateDisplay(response);
-    },
-    error: function(response) {
-        updateDisplay(response);
-    }
- });
+    $.ajax({
+        type: "POST",
+        crossDomain: true,
+        headers:{
+            'Accept' : 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        url: "/person-controller/create",
+        data: personData,
+        dataType: "JSON",
+        success: function(response) {
+            updateDisplay(response);
+        },
+        error: function(response) {
+            updateDisplay(response);
+        }
+     });
 }
 
 
